d99511910c0cb38e4e6d4cdaef2e31e751d926e8
lurpeeBurbee <gosilvershine@msn.com>
2024-10-10 19:25:36 +0300

    Background color adjustments

--- javascript-game/javascript.js
+++ javascript-game/javascript.js
@@ -1,26 +1,33 @@
 
 var htmlElm = document.getElementsByTagName("html")[0];
 var gameCanvas = document.getElementById("gameCanvas");
+var frontCanvas = document.getElementById("frontCanvas");
 
 // Hide both elements initially
 htmlElm.style.display = "none";
 gameCanvas.style.display = "none";
+frontCanvas.style.display = "none";
 
 document.addEventListener("DOMContentLoaded", function(event) {
     // Display both elements once the DOM is fully loaded
     htmlElm.style.display = "block";
     gameCanvas.style.display = "block";
+    frontCanvas.style.display = "block";
 });
 
 window.addEventListener('load', function() {
-    var gameCanvas = document.getElementById('gameCanvas');
+    var gamecanvas = document.getElementById('gameCanvas');
     var ufocanvas = this.document.getElementById('ufoCanvas');
+    var frontcanvas = this.document.getElementById('frontCanvas');
+
     var bgImage = new Image();
     bgImage.src ='/images/background-images/forest.jpeg'; 
 
     bgImage.onload = function() {
-        gameCanvas.style.width = bgImage.width + 'px';
-        gameCanvas.style.height = bgImage.height + 'px';
+        gamecanvas.style.width = bgImage.width + 'px';
+        gamecanvas.style.height = bgImage.height + 'px';
         ufocanvas.style.width = bgImage.width + 'px';
+        frontcanvas.style.width = bgImage.width + 'px';
+        frontcanvas.style.height = bgImage.height + 'px';
     };
 });
